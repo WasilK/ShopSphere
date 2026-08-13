63683e3613108c5a40357c6294ffef7b64d1275f
WasilK <khanmohammadwasil@gmail.com>
2026-08-13 14:43:04 +0530

    First commit

---
+++ src/test/java/com/wasil/ShopSphere/ShopSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wasil.ShopSphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopSphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
