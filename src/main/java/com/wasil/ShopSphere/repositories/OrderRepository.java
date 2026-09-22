55b9a0b1521b63e00d41c92a489cd918c773e534
WasilK <khanmohammadwasil@gmail.com>
2026-09-22 12:09:32 +0530

    Fixed abandoned payment issue.

--- src/main/java/com/wasil/ShopSphere/repositories/OrderRepository.java
+++ src/main/java/com/wasil/ShopSphere/repositories/OrderRepository.java
@@ -1,13 +1,16 @@
 package com.wasil.ShopSphere.repositories;
 
 import com.wasil.ShopSphere.model.Order;
+import com.wasil.ShopSphere.model.OrderStatus;
 import com.wasil.ShopSphere.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.Instant;
 import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findByUser(User user);
+    List<Order> findByOrderStatusAndOrderCreatedAtBefore(OrderStatus orderStatus, Instant cutoff);
 }
