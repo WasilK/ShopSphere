fdaf5697bbaf4bf82c0f8490faeef84a59f3cada
WasilK <khanmohammadwasil@gmail.com>
2026-09-24 16:13:29 +0530

    Trying to debug.

--- src/main/java/com/wasil/ShopSphere/security/JwtAuthenticationFilter.java
+++ src/main/java/com/wasil/ShopSphere/security/JwtAuthenticationFilter.java
@@ -90,6 +90,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             }
         }catch(Exception e){
             SecurityContextHolder.clearContext();
+            e.printStackTrace();
         }
         filterChain.doFilter(request, response);
     }
